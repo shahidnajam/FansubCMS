2d449c3410f148244c72a646d1c82afb6940b046
Hikaru Shindo <hikaru@animeownage.de>
2011-01-16 16:54:10 +0100

    fix test unit test
    Signed-off-by: Hikaru Shindo <hikaru@animeownage.de>

--- tests/DummyTest.php
+++ tests/DummyTest.php
@@ -3,7 +3,6 @@
 class DummyTest extends PHPUnit_Framework_TestCase {
     // just test if tests are configured correctly
     public function testTests() {
-        $this->assertEquals(true, true);
-        $this->fail('Oops, it seems there is something wrong with your PHPUnit setup!');
+        $this->assertEquals(true, true, 'Oops, it seems there is something wrong with your PHPUnit setup!');
     }
 }
