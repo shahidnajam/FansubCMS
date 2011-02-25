e6b2a74aca1618a3b3bd7a87c87175e4256ebf58
Hikaru Shindo <hikaru@animeownage.de>
2011-02-25 16:26:14 +0100

    All doctrine models now have Zend ACL resource identifiers

--- library/FansubCMS/Doctrine/Record.php
+++ library/FansubCMS/Doctrine/Record.php
@@ -1,9 +1,13 @@
 <?php
 class FansubCMS_Doctrine_Record extends Doctrine_Record implements Zend_Acl_Resource_Interface
 {   
+    /**
+     * Returns the string identifier for this model in the form module_m_model
+     * @see Zend_Acl_Resource_Interface::getResourceId()
+     * @return string
+     */
     public function getResourceId()
     {
-        // coming soon
-        return 'model'; // for now just return that it's a model...
+        return str_replace('_model_','_m_',strtolower(get_class($this)));
     }
 }
