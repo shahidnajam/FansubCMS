bca4e3491ab68ebc08e486ea685b660048a10778
Hikaru Shindo <hikaru@animeownage.de>
2011-04-11 16:51:35 +0200

    layout exporter

--- application/modules/cms/controllers/AdminController.php
+++ application/modules/cms/controllers/AdminController.php
@@ -30,7 +30,67 @@ class Cms_AdminController extends FansubCMS_Controller_Action
     {
         $this->_staticPath = realpath(APPLICATION_PATH . '/resource/static');
     }
-    
+
+    public function exportLayoutAction()
+    {
+        $layout = $this->request->getParam('layout');
+        $layoutMediaPath = HTTP_PATH . DIRECTORY_SEPARATOR . 'media' . DIRECTORY_SEPARATOR . $layout;
+        $layoutPath = $layoutMedia = APPLICATION_PATH . DIRECTORY_SEPARATOR . 'layouts' . DIRECTORY_SEPARATOR . $layout;
+        $delegatePaths = glob(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . '*' . DIRECTORY_SEPARATOR . 'delegates' . DIRECTORY_SEPARATOR . $layout);
+        $viewPaths = glob(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . '*' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . $layout);
+
+        if(empty($layout)) {
+            die('no layout given');
+        }
+
+        if(!is_dir($layoutPath)) {
+            die('layout path does not exist');
+        }
+
+        if(!is_dir($layoutMediaPath)) {
+            die('layout media path does not exist');
+        }
+
+        $tempPath = sys_get_temp_dir();
+        $tempDir = $tempPath . DIRECTORY_SEPARATOR . md5(time()) . '_style';
+        $tempLayoutDir = $tempDir . DIRECTORY_SEPARATOR . 'layout';
+
+        if(mkdir($tempDir)) {
+            // create base dirs
+            mkdir($tempLayoutDir);
+            mkdir($tempLayoutDir . DIRECTORY_SEPARATOR . 'application');
+            mkdir($tempLayoutDir . DIRECTORY_SEPARATOR . 'application' . DIRECTORY_SEPARATOR . 'layouts');
+            mkdir($tempLayoutDir . DIRECTORY_SEPARATOR . 'application' . DIRECTORY_SEPARATOR . 'modules');
+            mkdir($tempLayoutDir . DIRECTORY_SEPARATOR . 'public');
+            mkdir($tempLayoutDir . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'media');
+
+            // copy base
+            $this->_recurseCopy($layoutPath, $tempLayoutDir . DIRECTORY_SEPARATOR . 'application' . DIRECTORY_SEPARATOR . 'layouts' . DIRECTORY_SEPARATOR . $layout);
+            $this->_recurseCopy($layoutMediaPath, $tempLayoutDir . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'media' . DIRECTORY_SEPARATOR . $layout);
+
+            foreach($viewPaths as $vP) {
+                $cleanVP = str_replace(APPLICATION_PATH, '', $vP);
+                $this->_recurseCopy($vP, $tempLayoutDir . DIRECTORY_SEPARATOR . 'application' . DIRECTORY_SEPARATOR . $cleanVP);
+            }
+
+            foreach($delegatePaths as $dP) {
+                $cleanDP = str_replace(APPLICATION_PATH, '', $dP);
+                $this->_recurseCopy($dP, $tempLayoutDir . DIRECTORY_SEPARATOR . 'application' . DIRECTORY_SEPARATOR . $cleanDP);
+            }
+
+            $this->_zip($tempLayoutDir, $tempDir . DIRECTORY_SEPARATOR . $layout . '.zip');
+
+            header('Content-type: application/zip');
+            header('Content-Disposition: attachment; filename="'.$layout.'.zip"');
+
+            readfile($tempDir . DIRECTORY_SEPARATOR . $layout . '.zip');
+        } else {
+            die('could not create temporary dir');
+        }
+
+        die();
+    }
+
     public function liststaticAction() 
     {
         $this->view->pageTitle = $this->translate('cms_list_static_headline');
@@ -111,4 +171,68 @@ class Cms_AdminController extends FansubCMS_Controller_Action
             $this->_helper->redirector->gotoRoute(array('action'=>'liststatic','controller'=>'admin','module'=>'cms'));
         }
     }
+
+    /**
+     * Copy a folder recursive
+     * @param string $src
+     * @param string $dst
+     */
+    protected function _recurseCopy($src,$dst)
+    {
+        $dir = opendir($src);
+        @mkdir($dst, null, true);
+        while(false !== ( $file = readdir($dir)) ) {
+            if (( $file != '.' ) && ( $file != '..' )) {
+                if ( is_dir($src . '/' . $file) ) {
+                    $this->_recurseCopy($src . '/' . $file,$dst . '/' . $file);
+                }
+                else {
+                    copy($src . '/' . $file,$dst . '/' . $file);
+                }
+            }
+        }
+        closedir($dir);
+    }
+
+    /**
+     * Create a zip archive recursive
+     * @param string $source
+     * @param string $destination
+     * @return boolean
+     */
+    protected function _zip($source, $destination)
+    {
+        if (extension_loaded('zip') === true) {
+            if (file_exists($source) === true) {
+                    $zip = new ZipArchive();
+                    if ($zip->open($destination, ZIPARCHIVE::CREATE) === true) {
+                            $source = realpath($source);
+
+                            if (is_dir($source) === true) {
+                                    $files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+
+                                    foreach ($files as $file) {
+                                            $file = realpath($file);
+                                            if (is_dir($file) === true) {
+                                                    $zip->addEmptyDir(str_replace($source . DIRECTORY_SEPARATOR, '', $file . '/'));
+                                            }
+
+                                            else if (is_file($file) === true) {
+                                                    $zip->addFromString(str_replace($source . DIRECTORY_SEPARATOR, '', $file), file_get_contents($file));
+                                            }
+                                    }
+                            }
+
+                            else if (is_file($source) === true) {
+                                    $zip->addFromString(basename($source), file_get_contents($source));
+                            }
+                    }
+
+                    return $zip->close();
+            }
+        }
+
+        return false;
+    }
+
 }
