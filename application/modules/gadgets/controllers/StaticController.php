35dfb2a3fdeb9b4f7a57165f47df355598c2c2b0
Hikaru Shindo <hikaru@animeownage.de>
2011-11-29 13:07:21 +0100

    Gadget for rendering static pages

---
+++ application/modules/gadgets/controllers/StaticController.php
@@ -0,0 +1,32 @@
+<?php
+/*
+ * This file is part of FansubCMS.
+ *
+ *  FansubCMS is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  FansubCMS is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FansubCMS.  If not, see <http://www.gnu.org/licenses/>
+ */
+class Gadgets_StaticController extends FansubCMS_Controller_Action
+{
+    /**
+     * Render a static page as gadget
+     */
+    public function renderAction()
+    {
+        $title = $this->_request->getParam('page');
+        if(file_exists(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'resource' . DIRECTORY_SEPARATOR . 'static' . DIRECTORY_SEPARATOR . $title . '.html')) {
+            $this->view->content = file_get_contents(APPLICATION_PATH . DIRECTORY_SEPARATOR . 'resource' . DIRECTORY_SEPARATOR . 'static' . DIRECTORY_SEPARATOR . $title . '.html');
+        } else {
+            $this->view->content = 'Page does not exist';
+        }
+    }
+}
